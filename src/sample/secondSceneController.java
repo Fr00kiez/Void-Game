0414112aee5b5000ed2adbd77efa5fda3bbb3e82
Fr00kiez <phil7cookie@gmail.com>
2020-06-13 13:41:10 +0700

    Pop up update

--- src/sample/secondSceneController.java
+++ src/sample/secondSceneController.java
@@ -5,28 +5,58 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class secondSceneController implements Initializable {
     @FXML
     private Button okbutton;
 
+    @FXML
+    private TextField namefield;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) { }
 
     @FXML
     public void okbuttonOnAction(ActionEvent event) throws IOException {
-        Parent secondScene = FXMLLoader.load(getClass().getResource("theMission.fxml"));
-        Scene secondSceneScene = new Scene(secondScene);
-        Stage secondSceneStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        secondSceneStage.hide();
-        secondSceneStage.setScene(secondSceneScene);
-        secondSceneStage.show();
+        if (namefield.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Harap masukkan nama kamu!");
+
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Apakah nama kamu adalah " + namefield.getText() + " ?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                Parent secondScene = FXMLLoader.load(getClass().getResource("characterView.fxml"));
+                Scene secondSceneScene = new Scene(secondScene);
+                Stage secondSceneStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                secondSceneStage.hide();
+                secondSceneStage.setScene(secondSceneScene);
+                secondSceneStage.show();
+            } else {
+                alert.showAndWait();
+            }
+        }
+
+
+
+
     }
 }
